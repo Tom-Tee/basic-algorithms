c85e813fba2cf60775a19cff629ac8046ca43f91
Thomas Temple <tjt_84@hotmail.com>
2021-06-03 16:16:46 +1000

    basic algorithm of two indexes

---
+++ two_sums.rb
@@ -0,0 +1,21 @@
+def two_sum(nums, target)
+  array = []
+  i = 0
+  sum = 0
+    for i in nums do
+      if (i + nums[sum+1]) == target
+        array << sum
+        array << sum +1
+      else
+      sum += 1
+    end
+  end
+  array
+end
+
+
+array = [2, 7, 11, 15]
+array_2 = [3,2,4]
+
+
+p two_sum(array, 9)
