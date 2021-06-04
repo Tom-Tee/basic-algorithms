070817a853dd58a00fe6eca3c08f9fa01f8a75ad
Thomas Temple <tjt_84@hotmail.com>
2021-06-04 13:48:50 +1000

    solved

--- balls in a box/lib/balls.rb
+++ balls in a box/lib/balls.rb
@@ -3,13 +3,19 @@ def count_balls(low_limit, high_limit)
   array = (low_limit..high_limit).to_a
   x_array = []
   hash = {}
+  new_hash = {}
   array.each do |val|
     double = val.to_s.split("")
     mapped = double.map { |x| x.to_i }
-    hash[val] = mapped.inject(0){|sum,x| sum + x }
+    addition = mapped.inject(0){ |sum, x| sum + x }
+    hash[val] = addition
+    if new_hash.key?(hash[val])
+      new_hash[hash[val]] += 1
+    else
+      new_hash[hash[val]] = 1
+    end
   end
-  hash
+  new_hash.values.max
 end
-
 
 p count_balls(5, 15)
