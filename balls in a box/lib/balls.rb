a3e96ff50c2a990a2fa5cb2b4519d981f8e3982d
Thomas Temple <tjt_84@hotmail.com>
2021-06-04 12:00:10 +1000

    adding a hash to the algorithm

--- balls in a box/lib/balls.rb
+++ balls in a box/lib/balls.rb
@@ -1,5 +1,14 @@
 def count_balls(low_limit, high_limit)
+  one_array = (1..100).to_a
   array = (low_limit..high_limit).to_a
+  x_array = []
+  hash = {}
+  array.each do |val|
+    double = val.to_s.split("")
+    mapped = double.map { |x| x.to_i }
+    hash[val] = mapped.inject(0){|sum,x| sum + x }
+  end
+  hash
 end
 
 
