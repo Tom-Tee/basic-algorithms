ddbf45a5eaef789f0542a8b36d56338af0019a53
Thomas Temple <tjt_84@hotmail.com>
2021-06-09 13:00:53 +1000

    done with!

--- running-sum-of-array/lib/array_return.rb
+++ running-sum-of-array/lib/array_return.rb
@@ -4,23 +4,17 @@ def running_sum(nums)
   counter = 0
   new_number = 0
   # while counter != nums.length
-    new_number += (nums[counter] + nums[counter + 1])
-    array << new_number
-    counter += 1
-    new_number += (nums[counter + 1])
-    array << new_number
-    counter += 1
-    new_number += (nums[counter + 1])
-    array << new_number
-    counter += 1
-    new_number += (nums[counter + 1])
-    array << new_number
-    counter += 1
-    new_number += (nums[counter + 1])
-    array << new_number
-    counter += 1
+      new_number += (nums[counter] + nums[counter + 1])
+      array << new_number
+      counter += 1
+    while counter != nums.length-1
+      new_number += (nums[counter + 1])
+      array << new_number
+      counter += 1
+      p array
+    end
   # end
   array
 end
 
-p running_sum([1,1,1,1,1])
+
