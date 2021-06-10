2020d2d6501aa5493a9056d29a300c1f5c11499c
Thomas Temple <tjt_84@hotmail.com>
2021-06-10 14:13:20 +1000

    added regular expression

--- split_a_balanced_string/lib/balance_string.rb
+++ split_a_balanced_string/lib/balance_string.rb
@@ -1,15 +1,11 @@
 def balanced_string_split(s)
   # hash = { "R" => "L" }
   counter = 0
-  split = s.split(/(L+)/)
+  split = s.split(/(R+)/)
   split.each do |x|
-    if x.include?("L")
+    if x.include?("R")
       counter += 1
     end
   end
   counter
 end
-
-balanced_string_split("RLRRLLRLRL")
-
-p balanced_string_split("RLLLLRRRLR")
