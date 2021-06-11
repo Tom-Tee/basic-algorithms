f3d6b37459ab100e824794cef71d03b4593db24b
Thomas Temple <tjt_84@hotmail.com>
2021-06-11 20:09:10 +1000

    completed

--- kids_with_candies/lib/candies.rb
+++ kids_with_candies/lib/candies.rb
@@ -1,6 +1,7 @@
 def kids_with_candies(candies, extra_candies)
-
+    maximum = candies.max
+    candies.map { |x| (x + extra_candies) >= maximum }
 end
 
 
-p kids_with_candies([2,3,5,1,3], 3))
+p kids_with_candies([2, 3, 5, 1, 3], 3)
