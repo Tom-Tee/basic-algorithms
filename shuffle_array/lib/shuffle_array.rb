9db79239b52f61f325f1f67657ed3625295acafe
Thomas Temple <tjt_84@hotmail.com>
2021-06-13 19:20:21 +1000

    finished!"

--- shuffle_array/lib/shuffle_array.rb
+++ shuffle_array/lib/shuffle_array.rb
@@ -1,3 +1,12 @@
 def restore_string(s, indices)
-
+  new_hash = {}
+  s.split("").each_with_index do |val, index|
+    new_hash[indices[index]] = val
+  end
+  string = ""
+  new_hash.sort.each { |key, val| string << val }
+  string
 end
+
+p restore_string("aaiougrt", [4, 0, 2, 6, 7, 3, 1, 5])
+
