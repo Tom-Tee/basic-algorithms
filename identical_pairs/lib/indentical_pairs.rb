7bf6ceba897eb88cb47ef3e49123c61d73551dfb
Thomas Temple <tjt_84@hotmail.com>
2021-06-13 19:58:11 +1000

    mindblown

--- identical_pairs/lib/indentical_pairs.rb
+++ identical_pairs/lib/indentical_pairs.rb
@@ -1,14 +1,12 @@
 def num_identical_pairs(nums)
-    counter = 0
-    index = 1
-    for num in nums do
-
-       while index < nums.length
-        counter += 1 if num == num[index] || num < num[index]
-           index += 1
-       end
-
+    result = 0
+    nums.length.times do |i|
+        nums.length.times do |j|
+            result += 1 if nums[i] == nums[j] && i < j
+            p j
+        end
     end
+    result
 end
 
-
+p num_identical_pairs([1, 2, 3, 1, 1, 3])
