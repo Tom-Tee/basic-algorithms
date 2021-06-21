db2c14d95ef806ea0e3a98ecec4bc5fa3f946060
Thomas Temple <tjt_84@hotmail.com>
2021-06-22 09:03:20 +1000

    set up

--- count_matches/lib/count_matches.rb
+++ count_matches/lib/count_matches.rb
@@ -1,3 +1,11 @@
 def count_matches(items, rule_key, rule_value)
 
 end
+
+items = [["phone","blue","pixel"],["computer","silver","lenovo"],["phone","gold","iphone"]]
+
+ruleKey = "color"
+
+ruleValue = "silver"
+
+count_matches(items, ruleKey, ruleValue)
--- count_matches/spec/matches_test.rb
+++ count_matches/spec/matches_test.rb
@@ -1,5 +1,5 @@
 describe "Count Matches" do
   it "should take from the rule key and value and return if any matches are there any" do
-    expect(count_matches([["phone", "blue", "pixel"], ["computer", "silver", "lenovo"], ["phone", "gold", "iphone"]], ruleKey = "color", ruleValue = "silver")).to eq(["computer", "silver","lenovo"])
+    expect(count_matches([["phone", "blue", "pixel"], ["computer", "silver", "lenovo"], ["phone", "gold", "iphone"]], "color", "silver")).to eq(["computer", "silver","lenovo"])
     end
   end
