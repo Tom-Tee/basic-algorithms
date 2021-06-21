5d9ed4eb06a5a8c6a9efd886fbc6230c524e7ddf
Thomas Temple <tjt_84@hotmail.com>
2021-06-22 08:56:26 +1000

    added test

--- count_matches/spec/matches_test.rb
+++ count_matches/spec/matches_test.rb
@@ -1,1 +1,5 @@
-matches_test.rb
+describe "Count Matches" do
+  it "should take from the rule key and value and return if any matches are there" do
+    expect(count_matches([["phone","blue","pixel"],["computer","silver","lenovo"],["phone","gold","iphone"]], ruleKey = "color", ruleValue = "silver").to eq ["computer","silver","lenovo"])
+    end
+  end
