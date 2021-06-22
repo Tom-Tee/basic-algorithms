48ec391aa1f1ff55d04059d923f0df3ead8c0c1c
Thomas Temple <tjt_84@hotmail.com>
2021-06-23 09:37:04 +1000

    playing with rspec

--- count_matches/spec/matches_test.rb
+++ count_matches/spec/matches_test.rb
@@ -1,5 +1,8 @@
 describe "Count Matches" do
+    let(:arrays) { [["phone", "blue", "pixel"], ["computer", "silver", "lenovo"], ["phone", "gold", "iphone"]] }
+    let(:ruleKey) {[ "color" ]}
+    let(:ruleValue) {[ "silver" ]}
   it "should take from the rule key and value and return if any matches are there any" do
-    expect(count_matches([["phone", "blue", "pixel"], ["computer", "silver", "lenovo"], ["phone", "gold", "iphone"]], "color", "silver")).to eq(["computer", "silver","lenovo"])
+    expect(count_matches(arrays, ruleKey, ruleValue).to eq(["computer", "silver","lenovo"])
     end
   end
