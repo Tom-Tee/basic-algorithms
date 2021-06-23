15b0a9a1e99998b62dcc423fbf2fb8b70f50fdd5
Thomas Temple <tjt_84@hotmail.com>
2021-06-23 10:22:45 +1000

    completed

--- count_matches/spec/matches_test.rb
+++ count_matches/spec/matches_test.rb
@@ -3,8 +3,8 @@ require 'count_matches'
 describe "Count Matches" do
 
     let(:arrays) { [["phone", "blue", "pixel"], ["computer", "silver", "lenovo"], ["phone", "gold", "iphone"]] }
-    let(:rule_key) { ["color"] }
-    let(:rule_value) { ["silver"] }
+    let(:rule_key) { "color" }
+    let(:rule_value) { "silver" }
 
   it "should take from the rule key and value and return if any matches are there any" do
     expect(count_matches(arrays, rule_key, rule_value)).to eq(1)
